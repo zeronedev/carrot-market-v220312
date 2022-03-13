f0b26f790efb0a9c7abcfb4d2ff031b58efcc159
zeronedev <ok7375@gmail.com>
2022-03-13 13:31:45 +0900

    #4.3 Test Drive part Three (13:06)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-10 px-20 grid gap-10">
+    <div className="bg-slate-400 py-10 px-20 grid gap-10 min-h-screen">
       <div className="bg-white p-6 rounded-3xl shadow-xl">
         <span className="font-semibold text-3xl">Select Item</span>
         <div className="flex justify-between my-2">
@@ -31,7 +31,7 @@ const Home: NextPage = () => {
               <span className="text-sm text-gray-500">Orders</span>
               <span className="font-medium">340</span>
             </div>
-            <div className="h-24 w-24 bg-red-300 rounded-full" />
+            <div className="h-24 w-24 bg-blue-300 rounded-full" />
             <div className="flex flex-col items-center">
               <span className="text-sm text-gray-500">Spent</span>
               <span className="font-medium">$2,310</span>
@@ -43,8 +43,42 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white p-6 rounded-3xl shadow-xl"></div>
-      <div className="bg-white p-6 rounded-3xl shadow-xl"></div>
+      <div className="bg-white p-6 rounded-3xl shadow-xl">
+        <div className="flex mb-5 justify-between items-center">
+          <span>⬅️</span>
+          <div className="space-x-3">
+            <span>⭐️ 4.9</span>
+            <span className="shadow-md rounded-md p-1 ">💖</span>
+          </div>
+        </div>
+        <div className="bg-zinc-400 h-52 mb-5" />
+        <div className="flex flex-col ">
+          <span className="font-medium text-xl">Swoon Lounge</span>
+          <span className="text-xl text-gray-500">Chair</span>
+          <div className="flex justify-between items-center mt-3 mb-5">
+            <div>
+              <input type="radio" />
+              <input type="radio" />
+              <input type="radio" />
+            </div>
+            <div className="flex items-center space-x-5">
+              <button className="p-1.5 bg-blue-100 flex justify-center items-center rounded-lg aspect-square w-10 text-xl text-gray-500">
+                -
+              </button>
+              <span>1</span>
+              <button className="p-1.5 bg-blue-100 flex justify-center items-center rounded-lg aspect-square w-10 text-xl text-gray-500">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <span className="font-medium text-2xl">$450</span>
+            <button className="bg-blue-500 py-2 px-10 rounded-lg text-center text-sx text-white">
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
