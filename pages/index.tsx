c040ec2ca4401540a61fb29ab3ef1cdaeaab42fa
zeronedev <ok7375@gmail.com>
2022-03-13 15:32:22 +0900

    #4.6 Modifiers for Lists (07:00)

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,24 @@ const Home: NextPage = () => {
     <div className="grid min-h-screen gap-10 bg-slate-400 py-10 px-20">
       <div className="rounded-3xl bg-white p-6 shadow-xl">
         <span className="text-3xl font-semibold">Select Item</span>
-        <div className="my-2 flex justify-between">
-          <span className="text-gray-500 ">Grey Chair</span>
-          <span className="font-semibold">$1</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500">Grey Chair</span>
-          <span className="font-semibold">$1</span>
-        </div>
+        <ul>
+          {[1, 2, 3, 4].map((i) => (
+            <div
+              key={i}
+              className="my-2 flex justify-between  odd:bg-blue-500 even:bg-yellow-500"
+            >
+              <span className="text-gray-500">Grey Chair</span>
+              <span className="font-semibold">$19</span>
+            </div>
+          ))}
+        </ul>
+        <ul>
+          {["a", "b", "c", ""].map((c, i) => (
+            <li key={i} className="my-2 bg-red-500 py-2 empty:hidden ">
+              {c}
+            </li>
+          ))}
+        </ul>
         <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
           <span>Total</span>
           <span className="font-semibold">$9</span>
