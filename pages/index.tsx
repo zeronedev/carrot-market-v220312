64a5d7f17aa339fcda74c4ec3229bfa12f6864f3
zeronedev <zeronedev@zeronedevui-Macmini.local>
2022-03-13 18:49:58 +0900

    #4.8 More Modifiers: file input 버튼 수정, 중첩변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,11 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <form className="flex flex-col space-y-2 p-5">
-      <input
-        type="text"
-        required
-        placeholder="Username"
-        className="border p-1 peer border-gray-400 rounded-md"
-      />
-      <span className="hidden peer-invalid:block peer-invalid:text-red-500 ">
-        이 입력은 유효하지 않습니다
-      </span>
-      <span className="hidden peer-valid:block peer-valid:text-teal-500">
-        멋진 사용자 이름
-      </span>
-      <span className="hidden peer-hover:block peer-hover:text-amber-500">
-        안녕하세요
-      </span>
-      <input type="submit" value="Login" className="bg-white" />
-    </form>
+    <div className="flex flex-col space-y-2 p-5">
+      <p className="first-letter:text-7xl first-letter:hover:text-purple-400">
+        Hello everyone!
+      </p>
+    </div>
   );
 };
 
