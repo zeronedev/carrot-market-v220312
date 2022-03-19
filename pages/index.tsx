fca0c152ccdc4348028bd9b8d113337ddbe5e28b
zeronedev <zeronedev@zeronedevui-Macmini.local>
2022-03-20 07:41:30 +0900

    #5.1 Auth part One: function 만들어서 조건에 따라 클래스이름 지정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div className="dark:md:hover:bg-teal-400 bg-[url(`/vercel.svg`)]">
-      <h2 className="text-[1100px] text-[#e4a5f7]">Hello</h2>
-    </div>
-  );
+  return null;
 };
 
 export default Home;
