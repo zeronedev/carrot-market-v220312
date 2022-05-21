0f76c8698598e6a11d1bb6246bdb97511fbca438
zeronedev <ok7375@gmail.com>
2022-05-21 10:20:35 +0900

    #9.1 Accounts Logic:prisma upsert(), 찾는다, 없으면 만든다, 업데이트한다

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,7 +3,83 @@ import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+    where: {
+      ...payload,
+    },
+  });
+  console.log(user);
+  /*   if (phone) {
+    user = await client.user.upsert({
+      create: {
+        name: "Anonymous",
+        phone: +phone,
+      },
+      update: {
+        // ... in case it already exists, update
+      },
+      where: {
+        phone: +phone,
+      },
+    });
+  } else if (email) {
+    user = await client.user.upsert({
+      create: {
+        name: "Anonymous",
+        email,
+      },
+      update: {
+        // ... in case it already exists, update
+      },
+      where: {
+        email,
+      },
+    });
+  } */
+  // -------------
+  /*   if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (!user) {
+      console.log("찾지 못했음, 만들겠음.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          email,
+        },
+      });
+    }
+    console.log(user);
+  }
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+    if (user) console.log("user를 찾았습니다.");
+    if (!user) {
+      console.log("찾지 못했음, 만들겠음.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          phone: +phone,
+        },
+      });
+    }
+    console.log(user);
+  } */
+
   return res.status(200).end();
 }
 
