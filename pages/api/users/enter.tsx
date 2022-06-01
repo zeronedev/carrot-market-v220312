d4b0bcd8d19da8056f32b81a2d4ab182f2a0d103
zeronedev <ok7375@gmail.com>
2022-06-01 11:02:16 +0900

    #9.2 Token Logic: 유저학인 토큰생성 연결

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -4,82 +4,25 @@ import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
-    where: {
-      ...payload,
+  const user = phone ? { phone: +phone } : { email };
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
     },
   });
-  console.log(user);
-  /*   if (phone) {
-    user = await client.user.upsert({
-      create: {
-        name: "Anonymous",
-        phone: +phone,
-      },
-      update: {
-        // ... in case it already exists, update
-      },
-      where: {
-        phone: +phone,
-      },
-    });
-  } else if (email) {
-    user = await client.user.upsert({
-      create: {
-        name: "Anonymous",
-        email,
-      },
-      update: {
-        // ... in case it already exists, update
-      },
-      where: {
-        email,
-      },
-    });
-  } */
-  // -------------
-  /*   if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email: email,
-      },
-    });
-    if (!user) {
-      console.log("찾지 못했음, 만들겠음.");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          email,
-        },
-      });
-    }
-    console.log(user);
-  }
-  if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,
-      },
-    });
-    if (user) console.log("user를 찾았습니다.");
-    if (!user) {
-      console.log("찾지 못했음, 만들겠음.");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          phone: +phone,
-        },
-      });
-    }
-    console.log(user);
-  } */
-
+  console.log(token);
   return res.status(200).end();
 }
 
