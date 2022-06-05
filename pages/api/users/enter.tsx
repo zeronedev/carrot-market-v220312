956ebdd4691cf225ea12bc68e6e81c240ccdafe9
zeronedev <ok7375@gmail.com>
2022-06-05 19:06:21 +0900

    #9.4 Sending SMS:twilio 설치, 폰번호로 토큰 보내기 완료

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,17 @@
-import withHandler from "@libs/server/withHandler";
+import twilio from "twilio";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
@@ -22,8 +29,15 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       },
     },
   });
-  console.log(token);
-  return res.status(200).end();
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.TWILIO_MY_PHONE!,
+      body: `당신의 로그인 토큰은 ${payload} 입니다`,
+    });
+    console.log(message);
+  }
+  return res.json({ ok: true });
 }
 
 export default withHandler("POST", handler);
