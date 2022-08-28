a510eb4ce705ad30e0795c10a48795f8dade53c0
zeronedev <ok7375@gmail.com>
2022-08-28 13:05:08 +0900

    #11.8 Bound Mutations: 좋아요하트 토글, 같은 useSWR에거 받은 data변경 bound mutate

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -21,12 +21,14 @@ interface ItemDetailResponse {
 
 const ItemDetail: NextPage = () => {
   const router = useRouter();
-  const { data } = useSWR<ItemDetailResponse>(
+  const { data, mutate } = useSWR<ItemDetailResponse>(
     router.query.id ? `/api/products/${router.query.id}` : null
   );
   const [toggleFav] = useMutation(`/api/products/${router.query.id}/fav`);
   const onFavClick = () => {
     toggleFav({});
+    if (!data) return;
+    mutate({ ...data, isLiked: !data.isLiked }, false);
   };
   return (
     <Layout canGoBack>
