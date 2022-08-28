1c43f3c7bb87b5d087adbf060afa167069f5a789
zeronedev <ok7375@gmail.com>
2022-08-28 17:39:38 +0900

    #11.9 Unbound Mutations:다른화면의 데이타변경, unbound Mutate, useSWRConfig()

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -2,12 +2,12 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Layout from "@components/layout";
 import { useRouter } from "next/router";
-import useSWR from "swr";
+import useSWR, { useSWRConfig } from "swr";
 import Link from "next/link";
 import { Product, User } from "@prisma/client";
 import useMutation from "@libs/client/useMutation";
-import { clearScreenDown } from "readline";
 import { cls } from "@libs/client/utils";
+import useUser from "@libs/client/useUser";
 
 interface ProductWithUser extends Product {
   user: User;
@@ -20,15 +20,18 @@ interface ItemDetailResponse {
 }
 
 const ItemDetail: NextPage = () => {
+  const { user, isLoading } = useUser();
+  const { mutate } = useSWRConfig();
   const router = useRouter();
-  const { data, mutate } = useSWR<ItemDetailResponse>(
+  const { data, mutate: boundMutate } = useSWR<ItemDetailResponse>(
     router.query.id ? `/api/products/${router.query.id}` : null
   );
   const [toggleFav] = useMutation(`/api/products/${router.query.id}/fav`);
   const onFavClick = () => {
     toggleFav({});
     if (!data) return;
-    mutate({ ...data, isLiked: !data.isLiked }, false);
+    boundMutate((prev) => prev && { ...data, isLiked: !data.isLiked }, false);
+    // mutate(`/api/users/me`, (prev: any) => ({ ok: !prev.ok }), false);
   };
   return (
     <Layout canGoBack>
