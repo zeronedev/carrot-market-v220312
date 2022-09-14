bba772db4e18776ef598451d5e0af7b5d49edd04
zeronedev <ok7375@gmail.com>
2022-09-14 13:27:13 +0900

    #12.5 All Posts:상세페이지 답변바로적용, 전체리스트보기

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -11,26 +11,50 @@ async function handler(
     body: { question },
     session: { user },
   } = req;
-  const post = await client.post.create({
-    data: {
-      question,
-      user: {
-        connect: {
-          id: user?.id,
+  if (req.method === "POST") {
+    const post = await client.post.create({
+      data: {
+        question,
+        user: {
+          connect: {
+            id: user?.id,
+          },
         },
       },
-    },
-  });
-
-  res.json({
-    ok: true,
-    post,
-  });
+    });
+    res.json({
+      ok: true,
+      post,
+    });
+  }
+  if (req.method === "GET") {
+    const post = await client.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            wonderings: true,
+            answers: true,
+          },
+        },
+      },
+    });
+    res.json({
+      ok: true,
+      post,
+    });
+  }
 }
 
 export default withApiSession(
   withHandler({
-    methods: ["POST"],
+    methods: ["POST", "GET"],
     handler,
   })
 );
